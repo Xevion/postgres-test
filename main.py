97d563902ce88342da7b443a82611fe4a8123ada
Xevion <xevion@xevion.dev>
2023-04-23 08:54:52 -0500

    Add psycopg2-binary to Pipfile, setup main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from psycopg2 import connect
+from seed import main
+
+if __name__ == '__main__':
+    conn = connect(dbname='railway', user='postgres', password='8h9hxh5YeBwfqTCrxQQb',
+                   host='containers-us-west-174.railway.app', port='7238')
+
+    main(conn)
